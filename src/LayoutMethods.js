7d09e25bdefadf78654141e61285380b41c7e6b2
Miel2000 <fabien.orsn@gmail.com>
2020-03-26 16:53:13 +0100

    test

--- src/LayoutMethods.js
+++ src/LayoutMethods.js
@@ -1,13 +1,9 @@
 const layoutMethods = {
-
-    changeBg: function (obj) {
-    
-        console.log('on est bien dans le layoutMetyhods -> ', obj.color);
-        // obj.el.style.backgroundColor = obj.color;
-        // obj.el.classList.add(obj.color);
-
-    }
-
-}
+  changeBg: function(obj) {
+    console.log("on est bien dans le layoutMetyhods -> ", obj.color);
+    // obj.el.style.backgroundColor = obj.color;
+    // obj.el.classList.add(obj.color);
+  }
+};
 
 export default layoutMethods;
